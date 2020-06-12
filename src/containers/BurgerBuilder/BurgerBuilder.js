56407fdc56f64e7b55eac9fd90554d8478eb56b7
Vikrant Gupta <vikrantgupta@Vikrants-MacBook-Air.local>
2020-06-12 16:30:52 +0530

    first

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,29 +91,15 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading:true});
-        //alert('You continue!');
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Bhavya Oberoi',
-                address: {
-                    street: 'testStreet1',
-                    zipCode: '141001',
-                    country: 'India'
-                },
-                email: 'boo@mine.com'
-                
-            },
-            deliveryMethod:'fastest'
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json',order)
-        .then(response => {
-            this.setState({loading:false , purchasing:false});
-        })
-        .catch(error => {
-            this.setState({loading:false , purchasing:false});
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname : '/checkout',
+            search : '?' + queryString
         });
     }
 
